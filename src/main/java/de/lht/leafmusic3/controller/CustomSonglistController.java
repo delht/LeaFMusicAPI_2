e9813299a9545f28fe3512fdf303e8d3c50d518c
Thuan <thuanluong1230@gmail.com>
2025-04-22 23:11:48 +0700

    Thêm api kiểm tra bài hát có trong dscustom chưa

--- src/main/java/de/lht/leafmusic3/controller/CustomSonglistController.java
+++ src/main/java/de/lht/leafmusic3/controller/CustomSonglistController.java
@@ -2,11 +2,15 @@ package de.lht.leafmusic3.controller;
 
 import de.lht.leafmusic3.dto.customsonglist.CustomSonglistDTO;
 import de.lht.leafmusic3.dto.song.SongDTO;
+import de.lht.leafmusic3.repository.CustomSonglistRepository;
 import de.lht.leafmusic3.service.CustomSonglistService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,6 +18,7 @@ import java.util.List;
 public class CustomSonglistController {
 
     private final CustomSonglistService customSonglistService;
+    private final CustomSonglistRepository customSonglistRepository;
 
 //    @GetMapping("/{idList}")
 //    public List<CustomSonglistDTO> getCustomSonglistsbyIdList(@PathVariable int idList) {
@@ -36,6 +41,19 @@ public class CustomSonglistController {
     @DeleteMapping("/remove")
     public String removeSongFromList(@RequestParam int idList, @RequestParam int idSong) {
         return customSonglistService.removeSongFromList(idList, idSong);
+    }
+//    ==================
+
+    @GetMapping("/contains")
+    public ResponseEntity<Map<String, Boolean>> checkSongExistsInList(
+            @RequestParam int idList,
+            @RequestParam int idSong) {
+
+        boolean exists = customSonglistRepository.existsByIdListAndIdSong(idList, idSong);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("exists", exists);
+
+        return ResponseEntity.ok(response);
     }
 
 }
